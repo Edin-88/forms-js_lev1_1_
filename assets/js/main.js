817a4f8ae2cc4177f9f2e78dc6126cb685f2f28c
Your Name <selmanovic.edin1@gmail.com>
2023-01-11 13:27:52 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// Button in JS holen
+
+let numberInput = document.getElementById('number')
+
+let value = document.getElementById('value')
+
+const button = document.querySelector('button')
+
+function verdopplungAusfuehren() {
+    const userInput = number.value*2
+    value.innerHTML = userInput   
+}
